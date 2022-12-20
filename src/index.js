b289768411ac0dc388eb402d2736b89b760a48d6
Christian Kühn <c.kuehn@kuehlhaus.com>
2022-12-20 14:12:21 +0100

    [CLEANUP] cleanup unused files, restructure css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/index.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
